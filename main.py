479b9549c51f9ff32822501cb475d9eab651bb99
Esteban Flores <e2002florespulido@gmail.com>
2025-02-15 15:16:03 -0600

    Data Validation Component

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import CustomException
 from networksecurity.logging import logger
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__ == "__main__":
@@ -12,10 +13,18 @@ if __name__ == "__main__":
     dataIngestionConfig = DataIngestionConfig(trainingPipelineConfig)
     data_ingestion = DataIngestion(dataIngestionConfig)
   
-    logger.logging.info("Entered main function")
-    
+    logger.logging.info("Initiate the data ingestion.")
     dataIngestionArtifact = data_ingestion.initiate_data_ingestion()
+    logger.logging.info("Data ingestion completed.")
     print(dataIngestionArtifact)
+
+    dataValidationConfig = DataValidationConfig(trainingPipelineConfig)
+    data_validation = DataValidation(dataValidationConfig, dataIngestionArtifact)
+
+    logger.logging.info("Initiate the data validation.")
+    dataValidationArtifact =data_validation.initiate_data_validation()
+    logger.logging.info("Data validation completed.")
+    print(dataValidationArtifact)
     
   except Exception as e:
     raise CustomException(e, sys)
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,25 @@
+import yaml
+from networksecurity.exception import CustomException
+from networksecurity.logging import logging
+import os, sys
+import numpy as np
+import dill
+import pickle
+
+def read_yaml_file(file_path: str) -> dict:
+  try:
+    with open(file_path, 'rb') as yaml_file:
+      config = yaml.safe_load(yaml_file)
+    return config
+  except Exception as e:
+    raise CustomException(e, sys)
+  
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+  try:
+    if replace:
+      if os.path.exists(file_path):
+        os.remove(file_path)
+    with open(file_path, "w") as yaml_file:
+      yaml.dump(content, yaml_file)
+  except Exception as e:
+    raise CustomException(e, sys)
